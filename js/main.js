6f16a925a6b2bf4e63495ad69026ad9552cfdbda
Nerusin Sergey <s.nerusin@yandex.ru>
2022-06-02 07:57:45 +0300

    Header + mobile menu + js + кнопка переключения фона

--- js/main.js
+++ js/main.js
@@ -9,6 +9,9 @@ const menuContainer = document.querySelector('.container');
 /* находим селектор кнопки закрытия контейнера с меню для моб. устройств */
 const btnMenuClose = document.querySelector('.container__close');
 
+/* находим селектор кнопки переключения фона */
+const btnSwitch = document.querySelector('.container-footer__btn');   
+
 /* устанавливаем прослушиватель на кнопку меню "бургер" 
 при нажатии навешиваем класс container_opened - делааем меню видимым*/ 
 burgerBtn.addEventListener('click', () => {
@@ -19,4 +22,8 @@ burgerBtn.addEventListener('click', () => {
 при нажатии навешиваем класс container_opened - делааем меню видимым*/ 
 btnMenuClose.addEventListener('click', () => {
   menuContainer.classList.remove('container_opened');
+});
+
+btnSwitch.addEventListener('click', () => {
+  btnSwitch.classList.toggle('container-footer__btn_on');
 });
